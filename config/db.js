160882764406e806e920c60a7920a1c488931401
polinakhari <polina14.05@yandex.ru>
2020-06-13 20:15:36 +0300

    all

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(db, {
+        useNewUrlParser: true,        
+        useFindAndModify: false        
+      });
+      console.log("MongoDB connected...");
+    } catch (err) {
+      console.log(err.message);      
+    }
+  };
+  
+  module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+const path = require('path')
+const connectDB = require("./config/db")
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
+connectDB();
+
+//Создание движка
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: 'hbs'
+})
+//Регистрация движка по ключу hbs
+app.engine('hbs', hbs.engine)
+
+//Указание на использование шаблонизатора
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+
+
 app.listen(PORT,() => {
     console.log('Server is up and running')
 })
+
